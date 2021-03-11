89583de87762b7cb2d725bc5ed225d3931f496d9
JamesRichardsonSTI <JamesRichardsonSTI@users.noreply.github.com>
2021-03-11 13:33:18 -0500

    Revert "Merge branch 'main' of https://github.com/JamesRichardsonSTI/helloWorld into main"
    This reverts commit 9e69afc41f0826206787fe13187cfe1c2e975423, reversing
    changes made to 2356d273e69c73719ceec7d3ac7909a5f4e8d989.

--- README.txt
+++
@@ -1,3 +0,0 @@
-# helloWorld
-
-thanks for reading
--- helloWorld.java
+++ helloWorld.java
@@ -6,12 +6,11 @@ public static void main(String[] args){
 System.out.println("Hello World");
 
 
+System.out.println("Program modified but able to revert to origional version using GitHub Desktop");
 
 }
 
 
 
-
-}
 
 }
